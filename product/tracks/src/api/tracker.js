fa58106901f7b7834387d97f591fa3f0103ef431
Ten <Ten@host-145-181.inlacsl.westlafayette.in.us.clients.pavlovmedia.net>
2019-09-01 17:01:30 -0400

    add demo

--- product/tracks/src/api/tracker.js
+++ product/tracks/src/api/tracker.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { AsyncStorage } from "react-native";
 
 const instance = axios.create({
-  baseURL: "http://c181137c.ngrok.io"
+  baseURL: "http://ffc6d845.ngrok.io"
 });
 
 instance.interceptors.request.use(
